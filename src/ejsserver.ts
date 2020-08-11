2416bbecdbc865a8286cb306e1bc4e96e3055a33
hndrr <yozhik20@gmail.com>
2020-08-11 21:06:26 +0900

    ejs 変数の値を渡す

--- src/ejsserver.ts
+++ src/ejsserver.ts
@@ -3,11 +3,18 @@ import fs from 'fs';
 import ejs from 'ejs';
 
 //同期処理
-const template = fs.readFileSync('dist/template.ejs', 'utf8');
+const template = fs.readFileSync('src/template.ejs', 'utf8');
+//ejsページ内の情報
+const page_info = {
+    title: "ejsテンプレート",
+    content: "ejsテンプレートを使ったページ"
+};
 let server = http.createServer(getFromCrient);
 
+
 function getFromCrient(request: any, response: any): void {
-    let content = ejs.render(template);
+    //第2引数にオブジェクトを渡す
+    let content = ejs.render(template, page_info);
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(content);
     response.end();
