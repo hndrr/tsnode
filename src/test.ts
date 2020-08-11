98db5260533616194c9c53e022989d8d34c9e51f
hndrr <yozhik20@gmail.com>
2020-08-11 17:45:05 +0900

    test

---
+++ src/test.ts
@@ -0,0 +1,24 @@
+'use strict';
+/**
+ * 与えられた自然数の階乗を返す
+ * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
+ * @param {Number} n
+ * @returns {Number}
+ */
+function factorial(n: number) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+const assert = require('assert');
+assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1)}でした`);
+assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2)}でした`);
+assert.equal(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3)}でした`);
+assert.equal(
+    factorial(10),
+    3628800,
+    `10の階乗は3628800ですが、実際は${factorial(10)}でした`
+);
+console.log('すべてのテストを通過しました');
