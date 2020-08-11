a5a1a332585cb488b25f1ca67d3a9b177173a779
hndrr <yozhik20@gmail.com>
2020-08-11 19:50:24 +0900

    header出力

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,13 @@ import http from 'http';
 
 let server = http.createServer(
     (request, response) => {
-        response.end('<html><body><h1>Hello Node.js!</h1></body></html>');
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<!DOCTYPE html><html lang="ja">');
+        response.write('<head><meta charset="UTF-8">');
+        response.write('<title>hello</title></head>');
+        response.write('<body><h1>Hello Node.js!</h1></body>');
+        response.write('</body></html>');
+        response.end();
     }
 );
 server.listen(3000);
