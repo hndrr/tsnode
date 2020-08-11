0ff485779d8a4ee5f7100169cb553155aba43a89
hndrr <yozhik20@gmail.com>
2020-08-11 20:31:21 +0900

    func getFromCrient

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import http from 'http';
 import fs from 'fs';
 
-let server = http.createServer(
-    (request, response) => {
-        fs.readFile('dist/server.html', 'UTF-8',
-            (error, data) => {
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.write(data);
-                response.end();
-            }
-        );
-    }
-);
+let server = http.createServer(getFromCrient);
+
+function getFromCrient(request: any, response: any): void {
+    fs.readFile('dist/server.html', 'UTF-8',
+        (error, data) => {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(data);
+            response.end();
+        }
+    );
+}
+
 server.listen(3000);
 console.log('server Start');
