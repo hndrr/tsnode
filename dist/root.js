72e189f62c4cb73653978c45b1c628c8330351a2
hndrr <yozhik20@gmail.com>
2020-08-13 00:42:10 +0900

    query

--- dist/root.js
+++ dist/root.js
@@ -1,18 +1,20 @@
-import http from 'http';
-import fs from 'fs';
-import ejs from 'ejs';
-import url from 'url';
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
+const ejs_1 = __importDefault(require("ejs"));
+const url_1 = __importDefault(require("url"));
 //同期処理
-const template = fs.readFileSync('src/template.ejs', 'utf8');
-const style_css = fs.readFileSync('src/style.css', 'utf8');
-
-let server = http.createServer(getFromCrient);
+const template = fs_1.default.readFileSync('src/template.ejs', 'utf8');
+const style_css = fs_1.default.readFileSync('src/style.css', 'utf8');
+let server = http_1.default.createServer(getFromCrient);
 server.listen(3000);
 console.log('server Start');
-
-function getFromCrient(request: any, response: any): void {
-    let url_parse = url.parse(request.url, true); //第２引数trueでqueryオブジェクトを作成
+function getFromCrient(request, response) {
+    let url_parse = url_1.default.parse(request.url, true); //第２引数trueでqueryオブジェクトを作成
     switch (url_parse.pathname) {
         case "/":
             //第2引数にオブジェクトを渡す
@@ -25,14 +27,14 @@ function getFromCrient(request: any, response: any): void {
             if (query.msg != null) {
                 content_info.content += '<br> query.msg:' + query.msg;
             }
-            let content = ejs.render(template, content_info);
+            let content = ejs_1.default.render(template, content_info);
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(content);
             response.end();
             break;
         case "/other":
             //第2引数にオブジェクトを渡す
-            let content_other = ejs.render(template, {
+            let content_other = ejs_1.default.render(template, {
                 title: "other",
                 content: "otherページ"
             });
@@ -51,5 +53,4 @@ function getFromCrient(request: any, response: any): void {
             break;
     }
 }
-
-
+//# sourceMappingURL=root.js.map
