c3b329ddc1e178e73b729029c38f058e45be1cc8
hndrr <yozhik20@gmail.com>
2020-08-14 00:25:10 +0900

    form

--- dist/root.js
+++ dist/root.js
@@ -7,12 +7,15 @@ const http_1 = __importDefault(require("http"));
 const fs_1 = __importDefault(require("fs"));
 const ejs_1 = __importDefault(require("ejs"));
 const url_1 = __importDefault(require("url"));
+const querystring_1 = __importDefault(require("querystring"));
 //同期処理
-const template = fs_1.default.readFileSync('src/template.ejs', 'utf8');
-const style_css = fs_1.default.readFileSync('src/style.css', 'utf8');
+const template = fs_1.default.readFileSync("src/template.ejs", "utf8");
+const form = fs_1.default.readFileSync("src/form.ejs", "utf8");
+const boot_css = fs_1.default.readFileSync("node_modules/bootstrap/dist/css/bootstrap.min.css", "utf8");
+const style_css = fs_1.default.readFileSync("src/style.css", "utf8");
 let server = http_1.default.createServer(getFromCrient);
 server.listen(3000);
-console.log('server Start');
+console.log("server Start");
 function getFromCrient(request, response) {
     let url_parse = url_1.default.parse(request.url, true); //第２引数trueでqueryオブジェクトを作成
     switch (url_parse.pathname) {
@@ -20,37 +23,57 @@ function getFromCrient(request, response) {
             //第2引数にオブジェクトを渡す
             let content_info = {
                 title: "ejsテンプレート",
-                content: "ejsテンプレートを使ったページ"
+                content: "ejsテンプレートを使ったページ",
             };
-            //query_paramを取り出して
-            let query = url_parse.query;
-            if (query.msg != null) {
-                content_info.content += '<br> query.msg:' + query.msg;
-            }
-            let content = ejs_1.default.render(template, content_info);
-            response.writeHead(200, { 'Content-Type': 'text/html' });
+            let content = ejs_1.default.render(form, content_info);
+            response.writeHead(200, { "Content-Type": "text/html" });
             response.write(content);
             response.end();
             break;
         case "/other":
-            //第2引数にオブジェクトを渡す
-            let content_other = ejs_1.default.render(template, {
-                title: "other",
-                content: "otherページ"
-            });
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(content_other);
-            response.end();
+            response_other(request, response);
             break;
         case "/style.css":
-            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.writeHead(200, { "Content-Type": "text/css" });
             response.write(style_css);
             response.end();
             break;
-        default:
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end('Not Found');
+        case "/bootstrap.min.css":
+            response.writeHead(200, { "Content-Type": "text/css" });
+            response.write(boot_css);
+            response.end();
             break;
+        default:
+            response.writeHead(200, { "Content-Type": "text/plain" });
+            response.end("Not Found");
+            break;
+    }
+}
+function response_other(request, response) {
+    if (request.method == "POST") {
+        let body = "";
+        request.on("data", (data) => {
+            body += data;
+        });
+        request.on("end", () => {
+            let post_data = querystring_1.default.parse(body);
+            let msg = post_data.msg;
+            let content2 = ejs_1.default.render(template, {
+                title: "other",
+                content: msg,
+            });
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.write(content2);
+            response.end();
+        });
+    }
+    else {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(ejs_1.default.render(template, {
+            title: "other",
+            content: "Not Found",
+        }));
+        response.end();
     }
 }
 //# sourceMappingURL=root.js.map
--- src/root.ts
+++ src/root.ts
@@ -1,55 +1,81 @@
-import http from 'http';
-import fs from 'fs';
-import ejs from 'ejs';
-import url from 'url';
+import http from "http";
+import fs from "fs";
+import ejs from "ejs";
+import url from "url";
+import qs from "querystring";
 
 //同期処理
-const template = fs.readFileSync('src/template.ejs', 'utf8');
-const style_css = fs.readFileSync('src/style.css', 'utf8');
+const template = fs.readFileSync("src/template.ejs", "utf8");
+const form = fs.readFileSync("src/form.ejs", "utf8");
+const boot_css = fs.readFileSync(
+  "node_modules/bootstrap/dist/css/bootstrap.min.css",
+  "utf8"
+);
+const style_css = fs.readFileSync("src/style.css", "utf8");
 
 let server = http.createServer(getFromCrient);
 server.listen(3000);
-console.log('server Start');
+console.log("server Start");
 
 function getFromCrient(request: any, response: any): void {
-    let url_parse = url.parse(request.url, true); //第２引数trueでqueryオブジェクトを作成
-    switch (url_parse.pathname) {
-        case "/":
-            //第2引数にオブジェクトを渡す
-            let content_info = {
-                title: "ejsテンプレート",
-                content: "ejsテンプレートを使ったページ"
-            };
-            //query_paramを取り出して
-            let query = url_parse.query;
-            if (query.msg != null) {
-                content_info.content += '<br> query.msg:' + query.msg;
-            }
-            let content = ejs.render(template, content_info);
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(content);
-            response.end();
-            break;
-        case "/other":
-            //第2引数にオブジェクトを渡す
-            let content_other = ejs.render(template, {
-                title: "other",
-                content: "otherページ"
-            });
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(content_other);
-            response.end();
-            break;
-        case "/style.css":
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(style_css);
-            response.end();
-            break;
-        default:
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end('Not Found');
-            break;
-    }
+  let url_parse = url.parse(request.url, true); //第２引数trueでqueryオブジェクトを作成
+  switch (url_parse.pathname) {
+    case "/":
+      //第2引数にオブジェクトを渡す
+      let content_info = {
+        title: "ejsテンプレート",
+        content: "ejsテンプレートを使ったページ",
+      };
+      let content = ejs.render(form, content_info);
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+      break;
+    case "/other":
+      response_other(request, response);
+      break;
+    case "/style.css":
+      response.writeHead(200, { "Content-Type": "text/css" });
+      response.write(style_css);
+      response.end();
+      break;
+    case "/bootstrap.min.css":
+      response.writeHead(200, { "Content-Type": "text/css" });
+      response.write(boot_css);
+      response.end();
+      break;
+    default:
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.end("Not Found");
+      break;
+  }
 }
 
-
+function response_other(request: any, response: any): void {
+  if (request.method == "POST") {
+    let body = "";
+    request.on("data", (data: string) => {
+      body += data;
+    });
+    request.on("end", () => {
+      let post_data = qs.parse(body);
+      let msg = post_data.msg;
+      let content2 = ejs.render(template, {
+        title: "other",
+        content: msg,
+      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content2);
+      response.end();
+    });
+  } else {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(
+      ejs.render(template, {
+        title: "other",
+        content: "Not Found",
+      })
+    );
+    response.end();
+  }
+}
